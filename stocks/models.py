40df876387e3bc8b220812975ee3732fc01241cc
wanteatfruit <feasiblechart@163.com>
2022-07-22 19:50:25 +0800

    add one stock data

--- stocks/models.py
+++ stocks/models.py
@@ -2,14 +2,22 @@ from django.db import models
 from sqlalchemy import true
 
 # Create your models here.
+
+
 class A(models.Model):
-    current_date = models.CharField(max_length=10)
-    price = models.FloatField(null=true)
-    
-class B(models.Model):
-    current_date = models.CharField(max_length=10)
+    current_date = models.CharField(max_length=10, primary_key=true)
     price = models.FloatField(null=true)
 
+
+class B(models.Model):
+
+
+    current_date = models.CharField(max_length=10, primary_key=true)
+    price = models.FloatField(null=true)
+
+
 class C(models.Model):
-    current_date = models.CharField(max_length=10)
+
+
+    current_date = models.CharField(max_length=10, primary_key=true)
     price = models.FloatField(null=true)
